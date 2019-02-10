b998632f47205b2ee32862d49e8e0fe6687e11f4
wilmer142 <wilmercollege@gmail.com>
2019-02-10 14:13:43 -0500

    solve challenge 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hola Mundo</h1>');
+  res.send('<h1>HcUy6Re2LLBRtj</h1>');
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
