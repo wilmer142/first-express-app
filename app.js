62a3a1a104f93ef6f538c7d26ce46ff09b2b0540
wilmer142 <wilmercollege@gmail.com>
2019-02-10 14:51:08 -0500

    solve challenge 4

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hola ' + (Object.keys(req.query).length == 0 ? "desconocido" : req.query.nombre.length == 0 ? "desconocido" : req.query.nombre) + '!</h1>');
+app.get('/makers/:name', (req, res) => {
+	let name = req.params.name.charAt(0).toUpperCase() + req.params.name.slice(1);
+
+  res.send(`<h1>Hola ${name}!</h1>`);
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
